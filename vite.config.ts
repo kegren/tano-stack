c4bc311d11e252e0ca019967fe665205138ba89d
kegren <kennydamgren@gmail.com>
2025-11-15 14:21:16 +0100

    feat(nitro): add nitro package and update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   plugins: [
-    nitroV2Plugin({ preset: "bun" }),
+    nitro({ preset: "bun" }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
