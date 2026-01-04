affa7b7cf44d8f13a34f3397f4bb30ee3802c420
Kenny Damgren <kennydamgren@gmail.com>
2026-01-04 13:12:22 +0100

    refactor: remove unused shadcn components, restructured jobs into feature dir and add email verification job

--- src/lib/server/jobs/register.ts
+++
@@ -1,6 +0,0 @@
-import type { PgBoss } from "pg-boss";
-import { registerExampleJobs } from "./example";
-
-export const registerJobs = async (boss: PgBoss) => {
-  await registerExampleJobs(boss);
-};
